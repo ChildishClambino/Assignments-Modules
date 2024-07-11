cb31efd5d84ef2964c52ea12e334c7b7b1cf5d58
ChildishClambino <jacobgarcilazo@gmail.com>
2024-07-11 04:47:11 -0700

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,9 @@
+import text_utils as tu
+if __name__ == "__main__":
+    
+    
+    reverse = tu.reverse_string("thanks")
+    print(reverse)
+    
+    caps = tu.capitalize_string("jojo")
+    print(caps)
